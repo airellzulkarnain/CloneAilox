0391f1faa881dd5ef503f052fab6980c4259d7ba
airellzulkarnain <airell.zulkarnain@gmail.com>
2022-03-30 13:35:47 +0700

    initial commit

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        [StringLength(60,MinimumLength = 3)]
+        public string Title { get; set; } = string.Empty;
+
+        [Display(Name = "Release Date"), DataType(DataType.Date)]
+        public DateTime DateReleased { get; set; }
+        [RegularExpression(@"^[A-Z]+[A-Za-z\s]*$"), Required, StringLength(30)]
+        public string Genre { get; set; } = string.Empty;
+        [Range(20_000, 100_000), DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
+        public string Rating { get; set; } = string.Empty;
+    }
+}
---
+++ RazorPagesMovie/Models/SeedData.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Data;
+
+namespace RazorPagesMovie.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using(var context = new RazorPagesMovieContext(serviceProvider.GetRequiredService<DbContextOptions<RazorPagesMovieContext>>()))
+            {
+                if (context == null || context.Movie == null)
+                {
+                    throw new ArgumentNullException("Null RazorPagesMovieContext");
+                }
+
+                if (context.Movie.Any())
+                {
+                    return;
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "The Batman",
+                        DateReleased = DateTime.Parse("2022-3-4"),
+                        Genre = "Action Crime Drama",
+                        Price = 50_000M, 
+                        Rating = "R"
+                    }, new Movie
+                    {
+                        Title = "The Joker",
+                        DateReleased = DateTime.Parse("2019-10-4"),
+                        Genre = "Action Crime Drama",
+                        Price = 50_000M, 
+                        Rating = "R"
+                    }, new Movie
+                    {
+                        Title = "The Greatest Showman",
+                        DateReleased = DateTime.Parse("2017-12-7"),
+                        Genre = "Biography Musical Drama",
+                        Price = 40_000M, 
+                        Rating = "PG-13"
+                    }, new Movie
+                    {
+                        Title = "Free Guy",
+                        DateReleased = DateTime.Parse("2021-8-13"),
+                        Genre = "Action Adventure Comedy",
+                        Price = 45_000M, 
+                        Rating = "PG-13"
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
